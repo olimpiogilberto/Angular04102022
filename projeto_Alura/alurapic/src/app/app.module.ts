55dc4cc8d1e74cf9ca11794568a58fd607a929ac
olimpiogilberto <olimpiogilberto@hotmail.com>
2022-11-05 11:53:05 -0300

    commit atualizado
    projeto Alura - api funciona - alurapic não
    projeto Udemy - apenas inico, mas está on

---
+++ projeto_Alura/alurapic/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { PhotosModule } from './photos/photos.module';
+import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PhotosModule,
+    AppRoutingModule,
+    ErrorsModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
